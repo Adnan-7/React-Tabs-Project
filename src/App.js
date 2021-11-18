f2ff59b89d3bce63207e11c4e08a9a9011bfcf32
Adnan Khan <adnan7.zain@gmail.com>
2021-11-18 22:47:33 +0500

    fetching jobs data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
-  return (
-    <main>
-      <section className='title'>
-        <h1>hello wordl</h1>
-        <div className='underline'></div>
+  const [isloading, setIsloading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsloading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isloading) {
+    return (
+      <section className='section loading'>
+        <h1>Loading...</h1>
       </section>
-    </main>
+    );
+  }
+
+  const { id, title, dates, duties, company } = jobs[value];
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h1>experience</h1>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn container */}
+        {/* job info */}
+        <article className='job-info'>
+          <h2>{title}</h2>
+        </article>
+      </div>
+    </section>
   );
 }
 
