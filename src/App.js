cc1f7854a8eff686bcbbcff253e4ee73653fae82
Adnan Khan <adnan7.zain@gmail.com>
2021-11-18 22:08:28 +0500

    tabs project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main>
+      <section className='title'>
+        <h1>hello wordl</h1>
+        <div className='underline'></div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
